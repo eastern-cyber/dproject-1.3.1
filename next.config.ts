f02ebf8f83643feb30171cfb1fab895b168acd72
katawutp <katawut.paramee@gmail.com.com>
2025-09-24 07:10:46 +0700

    next.config.ts polygonscan&ipfs

--- next.config.ts
+++ next.config.ts
@@ -7,14 +7,41 @@
 // export default nextConfig;
 
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   env: {
+//     DATABASE_URL: process.env.DATABASE_URL,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// module.exports = nextConfig;
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'polygonscan.com',
+        pathname: '/token/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ipfs.io',
+        pathname: '/ipfs/**',
+      },
+    ],
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
